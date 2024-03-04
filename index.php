edf80dbd398f4b53dde4f363d7614e106a42335b
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-03-04 15:53:14 +0100

    feat: create class production

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+
+class Production{
+    public $title;
+    public $lang;
+    public $vote;
+    public function __construct($title, $lang, $vote){
+        $this->title = $title;
+        $this->lang = $lang;
+        $this->vote = $vote;
+    }
+
+}
